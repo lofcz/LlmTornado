196807a51b15ff3e7fb81bddc605c8cbac20242b
Bohumil Homola <bodahomola@gmail.com>
2025-01-22 14:01:15 +0100

    adding rest of the endpoints for VectorStoreFile

--- LlmTornado/VectorStores/VectorStoresEndpoint.cs
+++ LlmTornado/VectorStores/VectorStoresEndpoint.cs
@@ -47,7 +47,7 @@ public class VectorStoresEndpoint : EndpointBase
     public Task<HttpCallResult<VectorStore>> RetrieveVectorStoreAsync(string vectorStoreId,
         CancellationToken? cancellationToken = null)
     {
-        return HttpPostRaw<VectorStore>(Api.GetProvider(LLmProviders.OpenAi), Endpoint,
+        return HttpGetRaw<VectorStore>(Api.GetProvider(LLmProviders.OpenAi), Endpoint,
             GetUrl(Api.GetProvider(LLmProviders.OpenAi), $"/{vectorStoreId}"), cancellationToken);
     }
 
@@ -91,10 +91,11 @@ public class VectorStoresEndpoint : EndpointBase
         return HttpPostRaw<VectorStore>(Api.GetProvider(LLmProviders.OpenAi), Endpoint, GetUrl(Api.GetProvider(LLmProviders.OpenAi), $"/{vectorStoreId}"), request, cancellationToken);
         
     }
-    
+
     /// <summary>
     ///     Retrieves a list of vector stores. Available only for OpenAI
     /// </summary>
+    /// <param name="vectorStoreId"></param>
     /// <param name="query"></param>
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
@@ -103,5 +104,46 @@ public class VectorStoresEndpoint : EndpointBase
     {
         return HttpGetRaw<ListResponse<VectorStoreFile>>(Api.GetProvider(LLmProviders.OpenAi), Endpoint,
             GetUrl(Api.GetProvider(LLmProviders.OpenAi), $"/{vectorStoreId}/files"), cancellationToken);
+    }
+
+    ///  <summary>
+    /// 		Retrieves a vector store file. Available only for OpenAI
+    ///  </summary>
+    ///  <param name="vectorStoreId"></param>
+    ///  <param name="fileId"></param>
+    ///  <param name="cancellationToken"></param>
+    ///  <returns></returns>
+    public Task<HttpCallResult<VectorStoreFile>> RetrieveVectorStoreFileAsync(string vectorStoreId, string fileId,
+        CancellationToken? cancellationToken = null)
+    {
+        return HttpGetRaw<VectorStoreFile>(Api.GetProvider(LLmProviders.OpenAi), Endpoint,
+            GetUrl(Api.GetProvider(LLmProviders.OpenAi), $"/{vectorStoreId}/files/{fileId}"), cancellationToken);
+    }
+    
+    /// <summary>
+    ///     Modifies vector store file.
+    ///     <see cref="request" />.
+    /// </summary>
+    /// <param name="vectorStoreId">The ID of the assistant to modify.</param>
+    /// <param name="request"><see cref="VectorStore" />.</param>
+    /// <param name="cancellationToken">Optional, <see cref="CancellationToken" />.</param>
+    /// <returns><see cref="VectorStore" />.</returns>
+    public Task<HttpCallResult<VectorStoreFile>> CreateVectorStoreFileAsync(string vectorStoreId, CreateVectorStoreFileRequest request, CancellationToken? cancellationToken = null)
+    {
+        return HttpPostRaw<VectorStoreFile>(Api.GetProvider(LLmProviders.OpenAi), Endpoint, GetUrl(Api.GetProvider(LLmProviders.OpenAi), $"/{vectorStoreId}/files"), request, cancellationToken);
+    }
+
+    /// <summary>
+    ///     Delete a vector store. Available only for OpenAI
+    /// </summary>
+    /// <param name="vectorStoreId"></param>
+    /// <param name="fileId"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public async Task<HttpCallResult<bool>> DeleteVectorStoreFileAsync(string vectorStoreId, string fileId,
+        CancellationToken? cancellationToken = null)
+    {
+        HttpCallResult<DeletionStatus> status = await HttpAtomic<DeletionStatus>(Api.GetProvider(LLmProviders.OpenAi), Endpoint, HttpMethod.Delete, GetUrl(Api.GetProvider(LLmProviders.OpenAi), $"/{vectorStoreId}/files/{fileId}"), ct: cancellationToken).ConfigureAwait(ConfigureAwaitOptions.None);
+        return new HttpCallResult<bool>(status.Code, status.Response, status.Data?.Deleted ?? false, status.Ok, null);
     }
 }
