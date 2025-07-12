262106206413ea61a353a4aefffea8bdaa728f34
Matěj Štágl <stagl.matej@centrum.cz>
2025-07-12 11:58:19 +0200

    xai: grok4

--- src/LlmTornado/Common/ContentType.cs
+++ src/LlmTornado/Common/ContentType.cs
@@ -4,14 +4,3 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
 namespace LlmTornado;
-
-[JsonConverter(typeof(StringEnumConverter))]
-public enum ContentType
-{
-    [EnumMember(Value = "text")] 
-    Text,
-    [EnumMember(Value = "image_url")] 
-    ImageUrl,
-    [EnumMember(Value = "image_file")] 
-    ImageFile
-}
