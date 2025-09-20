2aa04d3b14e124f3d65b10f55f338c354244f7fe
Matěj Štágl <stagl.matej@centrum.cz>
2025-09-20 18:04:58 +0200

    xai: grok-4-fast-reasoning, grok-4-fast-non-reasoning

--- src/LlmTornado/Chat/Models/XAi/ChatModelXAiGrok4.cs
+++ src/LlmTornado/Chat/Models/XAi/ChatModelXAiGrok4.cs
@@ -21,11 +21,33 @@ public class ChatModelXAiGrok4 : IVendorModelClassProvider
     public readonly ChatModel V4 = ModelV4;
     
     /// <summary>
+    /// We're excited to release grok-4-fast, our latest advancement in cost-efficient reasoning models.
+    /// </summary>
+    public static readonly ChatModel ModelV4FastReasoning = new ChatModel("grok-4-fast-reasoning", LLmProviders.XAi, 2_000_000, [ "grok-4-fast", "grok-4-fast-reasoning-latest" ]);
+
+    /// <summary>
+    /// <inheritdoc cref="ModelV4FastReasoning"/>
+    /// </summary>
+    public readonly ChatModel V4FastReasoning = ModelV4FastReasoning;
+    
+    /// <summary>
+    /// We're excited to release grok-4-fast, our latest advancement in cost-efficient reasoning models.
+    /// </summary>
+    public static readonly ChatModel ModelV4FastNonReasoning = new ChatModel("grok-4-fast-non-reasoning", LLmProviders.XAi, 2_000_000, [ "grok-4-fast-non-reasoning-latest" ]);
+
+    /// <summary>
+    /// <inheritdoc cref="ModelV4FastNonReasoning"/>
+    /// </summary>
+    public readonly ChatModel V4FastNonReasoning = ModelV4FastNonReasoning;
+    
+    /// <summary>
     /// All Grok 4 models.
     /// </summary>
     public static List<IModel> ModelsAll => LazyModelsAll.Value;
 
-    private static readonly Lazy<List<IModel>> LazyModelsAll = new Lazy<List<IModel>>(() => [ModelV4]);
+    private static readonly Lazy<List<IModel>> LazyModelsAll = new Lazy<List<IModel>>(() => [
+        ModelV4, ModelV4FastReasoning, ModelV4FastNonReasoning
+    ]);
     
     /// <summary>
     /// <inheritdoc cref="ModelsAll"/>
